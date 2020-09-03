f32a396d060ffe239659fe0ab39fbe38dbe72bcc
Bora Açıcı <acicibora67@gmail.com>
2020-09-03 12:25:07 +0300

    b;h;u

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -25,7 +25,7 @@ export default {};
     top: 3vh;
     font-style: normal;
     font-weight: bold;
-    font-size: 3rem;
+    font-size: 1.5rem;
     line-height: 39px;
     color: #ffffff;
   }
@@ -53,7 +53,7 @@ export default {};
       margin-left: 38px;
       opacity: 0.3;
       transition: opacity 0.3s;
-      font-size: 2.5rem;
+      font-size: 1.1rem;
 
       &:hover {
         opacity: 1;
